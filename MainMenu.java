0e14215513264e9590d80d47ebd08ef54b2f187d
Oiko <oliver.chico@binus.ac.id>
2021-04-08 20:16:47 +0700

    add more menus

--- MainMenu.java
+++ MainMenu.java
@@ -6,7 +6,6 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.io.IOException;
 
 import javax.swing.BorderFactory;
