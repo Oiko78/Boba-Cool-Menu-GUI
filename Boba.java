270dbf7392de2ea8f6ffb8ecc8e4fb4f46dd5c90
Oiko <oliver.chico@binus.ac.id>
2021-04-09 13:16:34 +0700

    add Boba class

---
+++ Boba.java
@@ -0,0 +1,47 @@
+public class Boba {
+  private String code;
+  private String name;
+  private int price;
+  private int stock;
+
+
+  public Boba(String code, String name, int price, int stock) {
+    this.code = code;
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+  }
+
+  public String getCode() {
+    return this.code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPrice() {
+    return this.price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public int getStock() {
+    return this.stock;
+  }
+
+  public void setStock(int stock) {
+    this.stock = stock;
+  }
+
+}
